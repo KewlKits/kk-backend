e511f163de8cae944fef29299fd822f1eba018dd
Miles Olson <mpolson64@fb.com>
2018-07-16 10:46:56 -0700

    Added party model and new party post route

---
+++ models/Party.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const songSchema = new Schema({
+  uri: String,
+  createdAt: Date,
+  title: String,
+  artist: String,
+  album: String,
+  albumArtUrl: String.
+});
+
+const partySchema = new Schema({
+  name: String,
+  location: [Number],
+  createdAt: Date,
+  pool: [songSchema],
+  queue: [songSchema],
+});
+
+module.exports = mongoose.model('Party', partySchema);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const Party = require('models/party');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -5,11 +6,29 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+mongoose.connect(process.env.MONGODB_URI);
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({message: "working"});
 });
+
+router.route('/party')
+  .post((req, res) => {
+    const party = Party();
+    party.name = req.body.name;
+    party.location = [0, 0];
+    party.createdAt = new Date();
+
+    party.save((err) => {
+      if(err) {
+        console.log('Party not saved');
+        res.status(400).json(error: err);
+      }
+      res.status(200).json(party);
+    })
+  });
 
 app.use('/', router);
 
