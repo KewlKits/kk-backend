c144c7f29c833852f5169a5ed7e721004af3a497
Miles Olson <mpolson64@gmail.com>
2018-07-24 13:38:38 -0700

    Add songs to queue

--- server.js
+++ server.js
@@ -62,17 +62,17 @@ router.route('/party/:party_id')
       }
       res.status(200).json(party);
     });
-  })
-  .delete((req, res) => {
-    Party.remove({
-      _id: req.params.party_id,
-    }, (err, party) => {
-      if (err) {
-        res.status(400).json({ error: err });
-      }
-      res.status(200).json(party);
-    });
   });
+  // .delete((req, res) => {
+  //   Party.remove({
+  //     _id: req.params.party_id,
+  //   }, (err, party) => {
+  //     if (err) {
+  //       res.status(400).json({ error: err });
+  //     }
+  //     res.status(200).json(party);
+  //   });
+  // });
 
 router.route('/party/:party_id/pool/add')
   .put((req, res) => {
@@ -116,22 +116,22 @@ router.route('/party/:party_id/pool/add')
     });
   });
 
-router.route('/party/:party_id/pool/remove')
-  .put((req, res) => {
-    Party.findById(req.params.party_id, (err, party) => {
-      if (err) {
-        res.status(400).json({ error: err });
-      }
-      party.removeSongFromPool(req.body.song_id);
+// router.route('/party/:party_id/pool/remove')
+//   .put((req, res) => {
+//     Party.findById(req.params.party_id, (err, party) => {
+//       if (err) {
+//         res.status(400).json({ error: err });
+//       }
+//       party.removeSongFromPool(req.body.song_id);
 
-      party.save((saveErr) => {
-        if (saveErr) {
-          res.status(400).json({ error: saveErr });
-        }
-        res.status(200).json(party);
-      });
-    });
-  });
+//       party.save((saveErr) => {
+//         if (saveErr) {
+//           res.status(400).json({ error: saveErr });
+//         }
+//         res.status(200).json(party);
+//       });
+//     });
+//   });
 
 router.route('/party/:party_id/pool')
   .put((req, res) => {
@@ -156,35 +156,58 @@ router.route('/party/:party_id/queue/add')
       if (err) {
         res.status(400).json({ error: err });
       }
-      party.addSongToQueue(
-        req.body.uri, req.body.title, req.body.artist, req.body.album, req.body.albumArtUrl,
-      );
+      const song = new Song();
+      song.uri = req.body.uri;
+      song.title = req.body.title;
+      song.artist = req.body.artist;
+      song.album = req.body.album;
+      song.albumArtUrl = req.body.albumArtUrl;
 
-      party.save((saveErr) => {
-        if (saveErr) {
-          res.status(400).json({ error: saveErr });
+      song.party = req.params.party_id;
+      song.owner = req.body.owner;
+
+      song.save((songSaveErr) => {
+        if (songSaveErr) {
+          res.status(400).json({ error: songSaveErr });
         }
-        res.status(200).json(party);
+        party.addSongToQueue(song._id);
+        party.save((saveErr) => {
+          if (saveErr) {
+            res.status(400).json({ error: saveErr });
+          }
+          User.findById(req.body.owner, (userFindErr, user) => {
+            if (userFindErr) {
+              res.status(400).json({ error: userFindErr });
+            }
+            user.addSong(song._id);
+            user.save((userSaveErr) => {
+              if (userSaveErr) {
+                res.status(400).json({ error: userSaveErr });
+              }
+              res.status(200).json(party);
+            });
+          });
+        });
       });
     });
   });
 
-router.route('/party/:party_id/queue/remove')
-  .put((req, res) => {
-    Party.findById(req.params.party_id, (err, party) => {
-      if (err) {
-        res.status(400).json({ error: err });
-      }
-      party.removeSongFromQueue(req.body.song_id);
+// router.route('/party/:party_id/queue/remove')
+//   .put((req, res) => {
+//     Party.findById(req.params.party_id, (err, party) => {
+//       if (err) {
+//         res.status(400).json({ error: err });
+//       }
+//       party.removeSongFromQueue(req.body.song_id);
 
-      party.save((saveErr) => {
-        if (saveErr) {
-          res.status(400).json({ error: saveErr });
-        }
-        res.status(200).json(party);
-      });
-    });
-  });
+//       party.save((saveErr) => {
+//         if (saveErr) {
+//           res.status(400).json({ error: saveErr });
+//         }
+//         res.status(200).json(party);
+//       });
+//     });
+//   });
 
 router.route('/party/:party_id/queue/move')
   .put((req, res) => {
