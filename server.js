cb68288bfa4315e6f9dafff031f2193611ba09c0
Miles Olson <mpolson64@gmail.com>
2018-07-24 17:31:33 -0700

    No removals for now

--- server.js
+++ server.js
@@ -62,73 +62,73 @@ router.route('/party/:party_id')
       }
       res.status(200).json(party);
     });
-  })
-  .delete((req, res) => {
-    Party.findById(req.params.party_id, (err, party) => {
-      if (err) {
-        res.status(400).json({ error: err });
-      }
-      party.queue.forEach((songId) => {
-        Song.remove({
-          _id: songId,
-        }, (songDeleteErr, song) => {
-          if (songDeleteErr) {
-            res.status(400).json({ error: songDeleteErr });
-          }
-          User.findById(song.owner, (findUserErr, user) => {
-            if (findUserErr) {
-              res.status(400).json({ error: findUserErr });
-            }
-            user.removeSong(song._id);
-            user.removeUpvote(song._id);
-            user.removeDownvote(song._id);
-            user.save((userSaveErr) => {
-              if (userSaveErr) {
-                res.status(400).json({ error: userSaveErr });
-              }
-            });
-          });
-        });
-      });
-
-      party.pool.forEach((songId) => {
-        Song.remove({
-          _id: songId,
-        }, (songDeleteErr, song) => {
-          if (songDeleteErr) {
-            res.status(400).json({ error: songDeleteErr });
-          }
-          User.findById(song.owner, (findUserErr, user) => {
-            if (findUserErr) {
-              res.status(400).json({ error: findUserErr });
-            }
-            user.removeSong(song._id);
-            user.removeUpvote(song._id);
-            user.removeDownvote(song._id);
-            user.save((userSaveErr) => {
-              if (userSaveErr) {
-                res.status(400).json({ error: userSaveErr });
-              }
-            });
-          });
-        });
-      });
-
-      User.findById(party.owner, (err, user) => {
-        user.removeParty(party._id);
-        user.save((userSaveErr) => {
-          if (userSaveErr) {
-            res.status(400).json({ error: userSaveErr });
-          }
-          Party.remove({
-            _id: req.params.party_id,
-          }, (partyDeleteErr, deletedParty) => {
-            res.status(200).json(deletedParty);
-          });
-        });
-      });
-    });
   });
+  // .delete((req, res) => {
+  //   Party.findById(req.params.party_id, (err, party) => {
+  //     if (err) {
+  //       res.status(400).json({ error: err });
+  //     }
+  //     party.queue.forEach((songId) => {
+  //       Song.remove({
+  //         _id: songId,
+  //       }, (songDeleteErr, song) => {
+  //         if (songDeleteErr) {
+  //           res.status(400).json({ error: songDeleteErr });
+  //         }
+  //         User.findById(song.owner, (findUserErr, user) => {
+  //           if (findUserErr) {
+  //             res.status(400).json({ error: findUserErr });
+  //           }
+  //           user.removeSong(song._id);
+  //           user.removeUpvote(song._id);
+  //           user.removeDownvote(song._id);
+  //           user.save((userSaveErr) => {
+  //             if (userSaveErr) {
+  //               res.status(400).json({ error: userSaveErr });
+  //             }
+  //           });
+  //         });
+  //       });
+  //     });
+
+  //     party.pool.forEach((songId) => {
+  //       Song.remove({
+  //         _id: songId,
+  //       }, (songDeleteErr, song) => {
+  //         if (songDeleteErr) {
+  //           res.status(400).json({ error: songDeleteErr });
+  //         }
+  //         User.findById(song.owner, (findUserErr, user) => {
+  //           if (findUserErr) {
+  //             res.status(400).json({ error: findUserErr });
+  //           }
+  //           user.removeSong(song._id);
+  //           user.removeUpvote(song._id);
+  //           user.removeDownvote(song._id);
+  //           user.save((userSaveErr) => {
+  //             if (userSaveErr) {
+  //               res.status(400).json({ error: userSaveErr });
+  //             }
+  //           });
+  //         });
+  //       });
+  //     });
+
+  //     User.findById(party.owner, (err, user) => {
+  //       user.removeParty(party._id);
+  //       user.save((userSaveErr) => {
+  //         if (userSaveErr) {
+  //           res.status(400).json({ error: userSaveErr });
+  //         }
+  //         Party.remove({
+  //           _id: req.params.party_id,
+  //         }, (partyDeleteErr, deletedParty) => {
+  //           res.status(200).json(deletedParty);
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
 
 router.route('/party/:party_id/pool/add')
   .put((req, res) => {
